1f3d659924a6b681b773057420b7a6b05e5b076c
Ashton Fox <61950589+ashscottf@users.noreply.github.com>
2022-06-08 10:34:53 -0500

    Update Enter Patients Into AdvancedMD.py

--- Enter Patients Into AdvancedMD.py
+++ Enter Patients Into AdvancedMD.py
@@ -5,24 +5,15 @@
 
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.select import Select
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 from openpyxl import load_workbook
 from time import sleep
 from Patient import Patient
